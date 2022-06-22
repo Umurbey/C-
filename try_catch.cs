ac31d58c5989094142cf2a73f2e98d46b5fc2ce0
Onur Umur <52472245+Umurbey@users.noreply.github.com>
2022-06-22 17:49:56 +0300

    Add files via upload

---
+++ try_catch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                int b = int.Parse(Console.ReadLine());
+                
+                int c = a+b; // toplama
+
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine("Bir hata oluştu" + ex.message);
+            }
+            finally{
+                Console.WriteLine("İşlem tamamlandı");
+            }
+        }
+    }
+}
